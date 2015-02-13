ad69800cc3497d59640283f7956c4f403787d934
Da Moose <ahmed.musawir@hotmail.com>
2015-02-13 17:05:35 +0800

    DB rebuild. Not Production Ready

---
+++ jq_header.php
@@ -0,0 +1,5 @@
+  <header class="page-header">
+  <div class="container">
+  <h1><span class="glyphicon glyphicon-flash"></span>jQuery: The Gallery<br/> <small>Powered by jQuery Ajax and PHP</small></h1>
+  </div>
+  </header>
